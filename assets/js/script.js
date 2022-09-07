9c49f53168c3fda1c2d31cb51e11ba358633dfb5
hale-bopp97 <mattastik@gmail.com>
2022-09-07 14:13:50 -0700

    script.js started, and correction made to link in index.html

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+alert('whooo...')
+
+var questions = [
+
+    {
+        question: 'what is 2 + 2 = ?',
+        answer: '4',
+        options: ['4', '3', '2', '1']
+    },
+    
+    {
+        question: 'what is 2 * 3 = ?',
+        answer: '6',
+        options: ['2', '4', '6', '8']
+    },
+
+    {
+        question: 'what is 10^2 = ?',
+        answer: '6',
+        options: ['1000', '100', '10', '1']
+    },
+
+    {
+        question: 'what\'s up?',
+        answer: 'not much',
+        options: ['I don\'t have change', 'what?', 'you too!', 'not much']
+    },
+
+    {
+        question: 'what car should I get?',
+        answer: 'toyota... it\'s always toyota',
+        options: ['my ford cvt is disintegrating', '4', 'toyota... it\'s always toyota', 'My nissan\'s wiring harness is moody:(']
+    },
+
+];
+
+
